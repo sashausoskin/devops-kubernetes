27bf4c377ff7dd311e1a0f950eae1728b3f0c5f4
Sasha Usoskin <sasha.usoskin@sievo.com>
2026-09-07 15:10:39 +0300

    Completed exercise 1.13

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react'
 import './App.css'
+import { Button, Card, TextField } from '@mui/material'
+
+const defaultTodos = [
+  'Learn Kubernetes basics',
+  'Deploy application to cluster',
+  'Configure persistent volumes'
+]
 
 export const App = () => {
+  const [todos, setTodos] = useState(defaultTodos)
+  const [inputValue, setInputValue] = useState('')
+
+  const onSubmit = () => {
+    setTodos([...todos, inputValue])
+    setInputValue('')
+  }
+
   return <>
     <div className='mainContainer'>
       <h1>Todo App</h1>
       <img src={import.meta.env.DEV ? 'http://localhost:3002/image' : '/image'} width='800px'/>
       <br />
-      DevOps with Kubernetes 2026
+      <div className='inputContainer'>
+        <TextField slotProps={{ htmlInput: { maxLength: 120 }}} id='todo-input' variant='outlined' value={inputValue} onChange={(e) => setInputValue(e.currentTarget.value)}/>
+        <Button variant='contained' onClick={onSubmit}>Submit</Button>
+      </div>
+      <div className='todosContainer'>
+      {todos.map((todo) => 
+        <Card className='todoCard'>{todo}</Card>
+      )}
+      </div>
     </div>
   </>
 }
