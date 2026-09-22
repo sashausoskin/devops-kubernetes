e7ed756b66a3f2e4aca239a20a362826cc3ff7cc
Sasha Usoskin <sasha.usoskin@sievo.com>
2026-09-22 17:45:47 +0300

    Completed exercise 2.5
    Got some help from Claude with the file mounting

--- log-output/index.ts
+++ log-output/index.ts
@@ -1,11 +1,27 @@
 import * as uuid from 'uuid'
 import axios from 'axios'
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const string = uuid.v7();
 
 const PORT = process.env.PORT || 3001
 const pingPongUrl = process.env.PING_PONG_URL || 'http://localhost:3000'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const fileLocation = path.join(__dirname, 'config', 'information.txt')
+const getFileContents = () => {
+    if (!fs.existsSync(fileLocation)) return 'No information available!'
+
+    const fileContents = fs.readFileSync(fileLocation)
+    return `file content: ${fileContents}`
+}
+
+const getMessage = () => {
+    return `env variable: MESSAGE=${process.env.MESSAGE || 'undefined'}`
+}
 
 const getStatus = () => {
     const timeStampDate = new Date(Date.now())
@@ -21,7 +37,7 @@ const getPingPongs = async () => {
 const app = express()
 
 app.get('/log', async (req, res) => {
-    res.send(`${getStatus()} <br/> ${await getPingPongs()}`)
+    res.send(`${getFileContents()} <br/> ${getMessage()} <br/> ${getStatus()} <br/> ${await getPingPongs()}`)
 })
 
 app.listen(PORT, () => {
