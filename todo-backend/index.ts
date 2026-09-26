918710ab3d2bb7da18379c9cb6d28fbfb8910a55
Sasha Usoskin <sasha.usoskin@sievo.com>
2026-09-26 17:01:02 +0300

    Completed exercise 2.8
    Also moved the db Docker compose to root and fixed an issue from a previous exercise

--- todo-backend/index.ts
+++ todo-backend/index.ts
@@ -1,17 +1,34 @@
 import express from 'express'
 import cors from 'cors'
+import { Client, Pool } from 'pg'
+import dotenv from 'dotenv'
 
-let todos: string[] = []
+dotenv.config()
 
 const PORT = process.env.PORT || 3003
+
+const dbPool = await new Pool({connectionString: process.env.POSTGRES_URL}).connect()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/todos', (req, res) => {
-    return res.json(todos)
+const initDb = async () => {
+    await dbPool.query('CREATE TABLE IF NOT EXISTS todos (todo VARCHAR(128));')
+}
+
+const getTodos = async () => {
+    const res = await dbPool.query('SELECT todo FROM todos;')
+    return res.rows.map((row) => row.todo)
+}
+
+const addTodo = async (todo: string) => {
+    dbPool.query('INSERT INTO todos (todo) VALUES ($1::text);', [todo])
+}
+
+app.get('/todos', async (req, res) => {
+    return res.json(await getTodos())
 })
 
 app.post('/todos', (req, res) => {
@@ -21,10 +38,11 @@ app.post('/todos', (req, res) => {
         return res.status(400).send('Make sure to only send a single string to add.')
     }
 
-    todos.push(todo)
+    addTodo(todo)
     return res.status(200).json(todo)
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`App listening on port ${PORT}`)
+    await initDb()
 })
